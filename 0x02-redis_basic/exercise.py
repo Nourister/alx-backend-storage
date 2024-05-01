a63ef19762b468f74e1a1de96c71108267f6c4f0
nourister <nouristerjuma@gmail.com>
2024-05-01 13:02:01 +0000

    0x02-redis_basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 
 import redis
 import uuid
-from typing import Union, Callable
+from typing import Callable
 from functools import wraps
 
 class Cache:
@@ -19,7 +19,27 @@ class Cache:
             return method(self, *args, **kwargs)
         return wrapper
 
+    @staticmethod
+    def call_history(method: Callable) -> Callable:
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            input_key = method.__qualname__ + ":inputs"
+            output_key = method.__qualname__ + ":outputs"
+
+            # Store input arguments
+            self._redis.rpush(input_key, str(args))
+
+            # Execute the wrapped function to retrieve the output
+            output = method(self, *args, **kwargs)
+
+            # Store the output
+            self._redis.rpush(output_key, str(output))
+
+            return output
+        return wrapper
+
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
@@ -38,3 +58,4 @@ class Cache:
 
     def get_int(self, key: str) -> Union[int, None]:
         return self.get(key, fn=int)
+
